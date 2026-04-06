4054c97eb1e3df75c325cbcec66fac1cf36223cc
Roberto Leyva Guerrero <roberto.leyva2301@alumnos.ubiobio.cl>
2026-04-06 15:04:26 -0400

    Fix GitHub Pages 404 and base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://RJLeyvag549.github.io',
+  base: '/Landing_Page_1',
+});
